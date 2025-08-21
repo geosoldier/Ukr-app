1adeacf4fecabc0043957fdd66885e3d1b0cdb99
geosoldier <89037538+geosoldier@users.noreply.github.com>
2025-08-21 20:05:56 +0100

    Initial Commit

---
+++ Ukrainian/UkrainianApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkrainianApp.swift
+//  Ukrainian
+//
+//  Created by Eric Adams on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct UkrainianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
